f5d739202016df22e74809b71abe779a50805236
Junaid Musla <junaidmusla@gmail.com>
2025-07-30 10:35:57 +0530

    Defining ShellView SideList and SideDetail

--- StockRadarSln/SRWpfClient/ViewModel/ShellViewModel.cs
+++ StockRadarSln/SRWpfClient/ViewModel/ShellViewModel.cs
@@ -14,14 +14,22 @@ namespace SRWpfClient.ViewModel
     /// data and commands to the shell's UI components.</remarks>
     public class ShellViewModel : ViewModelBase
     {
+        #region Private Fields
+
         private StockNotificationServiceProxy _stockNotificationServiceProxy;
 
         private ObservableCollection<StockData> _stockCollection;
 
+        private ObservableCollection<StockData> _subscribedStockCollection;
+
+        #endregion
+
+        #region Public Properties
+
         public ObservableCollection<StockData> StockCollection
         {
             get { return _stockCollection; }
-            set 
+            set
             {
                 if (_stockCollection != value)
                 {
@@ -31,11 +39,36 @@ namespace SRWpfClient.ViewModel
             }
         }
 
+        public ObservableCollection<StockData> SubscribedStockCollection
+        {
+            get { return _subscribedStockCollection; }
+            set
+            {
+                if (_subscribedStockCollection != value)
+                {
+                    _subscribedStockCollection = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShellViewModel"/> class.
+        /// </summary>
         public ShellViewModel()
         {
             _stockNotificationServiceProxy = SR.WpfClient.Factories.ProxyFactory.GetStockNotificationServiceProxy(SRConstants.Scheme_NETTCP);
             InitialiseStockCollection();
+            InitialiseSubscribedStockCollection();
         }
+
+        #endregion
+
+        #region Private Methods
 
         private void InitialiseStockCollection()
         {
@@ -49,5 +82,19 @@ namespace SRWpfClient.ViewModel
                 StockCollection = new ObservableCollection<StockData>();
             }
         }
+
+        private void InitialiseSubscribedStockCollection()
+        {
+            SubscribedStockCollection = new ObservableCollection<StockData>();
+            SubscribedStockCollection.CollectionChanged += SubscribedStockCollection_CollectionChanged;
+        }
+
+        private void SubscribedStockCollection_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
     }
 }
